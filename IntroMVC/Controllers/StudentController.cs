990ec7a49c4f84adf1f06248acd9038ba2c5a36a
Ashik Rahman <ashrhmn99@gmail.com>
2022-02-03 19:07:00 +0600

    add database connection for student list

---
+++ IntroMVC/Controllers/StudentController.cs
@@ -0,0 +1,49 @@
+﻿using IntroMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+
+namespace IntroMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+
+            SqlConnection conn = new SqlConnection("Data Source=1.10.11.107;Initial Catalog=IntroMVP;Persist Security Info=True;User ID=sa;Password=mssql-2019");
+            conn.Open();
+            string query = "select * from students";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<Student> students = new List<Student>();
+            while (reader.Read())
+            {
+                Student student = new Student();
+                student.ID = reader["ID"].ToString();
+                student.Name = reader["Name"].ToString();
+                student.Department = reader["Department"].ToString();
+                students.Add(student);
+            }
+            return View(students);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Submit(Student s)
+        {
+            SqlConnection conn = new SqlConnection("Data Source=1.10.11.107;Initial Catalog=IntroMVP;Persist Security Info=True;User ID=sa;Password=mssql-2019");
+            conn.Open();
+            string query = "insert into students (id,name, department) values('" + s.ID + "','" + s.Name + "','" + s.Department + "')";
+            SqlCommand cmd = new SqlCommand(query,conn);
+            int rowsAffected = cmd.ExecuteNonQuery();
+            conn.Close();
+            return RedirectToAction("Index","Student");
+        }
+    }
+}
---
+++ IntroMVC/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroMVC.Models
+{
+    public class Student
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+    }
+}
